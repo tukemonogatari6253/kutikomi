175f9fc993339095037d3e21d82375ec0506f0e0
tukemonogatari6253 <110277953+tukemonogatari6253@users.noreply.github.com>
2025-05-04 15:24:30 +0900

    Update protected.js

--- pages/protected.js
+++ pages/protected.js
@@ -6,10 +6,11 @@ export default function Protected({ data }) {
   return (
     <div>
       <h1>Protected Page</h1>
+    　<p>こちらは会員しか閲覧することができません。</p>
       <pre>{JSON.stringify(data, null, 2)}</pre>
       <a href="/api/auth/logout">ログアウト</a>
       <br />
-      <Link href="/">会員ページにいく</Link>
+      <Link href="/">会員ホームにいく</Link>
     </div>
   );
 }
