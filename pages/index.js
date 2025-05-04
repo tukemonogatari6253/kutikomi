abd9073c48a6bc716177f644d90364a0494a196d
tukemonogatari6253 <110277953+tukemonogatari6253@users.noreply.github.com>
2025-05-04 16:37:03 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useUser } from '@auth0/nextjs-auth0/client';
 import Link from 'next/link';
-import Head from 'next/head'; // 追加
+import Head from 'next/head';
 
 export default function Home() {
   const { user, error, isLoading } = useUser();
@@ -10,25 +10,28 @@ export default function Home() {
 
   return (
     <div>
-    <Head>
-<title>ホーム - こんにちは。</title>
-<meta name="description" content="Auth0認証を使ったNext.jsアプリ" />
-</Head>
+      <Head>
+        <title>ホーム - こんにちは。</title>
+        <meta name="description" content="Auth0認証を使ったNext.jsアプリ" />
+      </Head>
       {!user ? (
-        <h1>こんにちは。まずはログインしよう</h1>
-        <Link href="/api/auth/login">ログイン</Link>
+        <>
+          <h1>こんにちは。まずはログインしよう</h1>
+          <Link href="/api/auth/login">ログイン</Link>
+        </>
       ) : (
         <>
           <h1>ログインできました。こんにちは, {user.name}</h1>
           <Link href="/api/auth/logout">ログアウト</Link>
           <br />
           <Link href="/protected">会員ページにいく</Link>
-         <Link href="/reviews">口コミページへ</Link>
+          <Link href="/reviews">口コミページへ</Link>
         </>
       )}
     </div>
   );
 }
+
 export const getServerSideProps = async () => {
   return { props: {} };
 };
